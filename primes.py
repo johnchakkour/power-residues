d7437104a99b1cc565d86d4cf457e121bb0c60e2
johnchakkour <53233282+johnchakkour@users.noreply.github.com>
2026-03-29 17:02:21 -0400

    Added verify() test

--- primes.py
+++ primes.py
@@ -1,13 +1,15 @@
 from periods import *
 
 if __name__ == '__main__':
+    verify(3, lambda a, b: a**2 + 27*b**2, 1000,
+           eq_label="q = a² + 27b²")
+    """
     bound = int(input("Scan primes q up to: "))
     print(f"{'e':>5} "
           f"{'primes q = 1 mod e such that 2 is an e-th power mod q':>90}")
     print("-" * 68)
     for e in primerange(3, 100):
         print(f"{e:>5} {str(scan(e, bound)):>90}")
-    """
     try:
         e = int(input("Enter a prime e: "))
         bound = int(input("Scan primes q up to: "))
